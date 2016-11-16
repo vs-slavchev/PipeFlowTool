87a12abbe017ab550d35cb921c28aa2b21826a9b
Veselin Slavchev <vs_slavchev@abv.bg>
2016-11-16 21:02:33 +0100

    Add mouse listener

--- src/Main.java
+++ src/Main.java
@@ -5,32 +5,37 @@ import java.awt.GraphicsEnvironment;
 import java.awt.GraphicsDevice;
 import java.awt.Rectangle;
 import java.awt.Dimension;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class Main extends Canvas {
+public class Main extends Canvas implements MouseListener {
 
-    private int SCREEN_WIDTH;
-    private int SCREEN_HEIGHT;
+    private int screenWidth;
+    private int screenHeight;
 
     private Main() {
         GraphicsEnvironment env = GraphicsEnvironment.getLocalGraphicsEnvironment();
         GraphicsDevice device = env.getScreenDevices()[0];
-        Rectangle SCREEN_RECTANGLE = device.getDefaultConfiguration().getBounds();
-        SCREEN_WIDTH = SCREEN_RECTANGLE.width;
-        SCREEN_HEIGHT = SCREEN_RECTANGLE.height;
+        Rectangle screenRectangle = device.getDefaultConfiguration().getBounds();
+        screenWidth = screenRectangle.width;
+        screenHeight = screenRectangle.height;
 
         JFrame container = new JFrame("PipeFlowTool");
         JPanel panel = (JPanel) container.getContentPane();
-        panel.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+        panel.setPreferredSize(new Dimension(screenWidth, screenHeight));
         panel.setLayout(null);
         panel.add(this);
 
-        setBounds(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+        setBounds(0, 0, screenWidth, screenHeight);
         //setLocation(0, 0);
 
+        container.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         container.pack();
         container.setResizable(false);
         container.setVisible(true);
         requestFocus();
+
+        addMouseListener(this);
     }
 
 	public static void main(String[] args) {
@@ -39,4 +44,29 @@ public class Main extends Canvas {
 
 	private void run() {
 	}
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        System.out.println("Clicked: " + e.getX() + ":" + e.getY());
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
